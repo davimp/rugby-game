6b5a7effe056ad3c1a0670f72a3e0c9a4772d619
davimp <davimp2009@gmail.com>
2022-03-26 18:24:50 -0300

    Add defender strategy

--- src/defender.c
+++ src/defender.c
@@ -1,5 +1,6 @@
 // Standard headers
 #include <stdio.h>
+#include <stdlib.h>
 
 // Internal headers
 #include "direction.h"
@@ -12,18 +13,99 @@
 // Macros
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
 
+
+/*----------------------------------------------------------------------------*/
+/*                              PRIVATE FUNCTIONS                             */
+/*----------------------------------------------------------------------------*/
+
+// Utils
+
+bool right(position_t defender, position_t target) {
+  return target.i == defender.i && target.j > defender.j;
+}
+
+bool left(position_t defender, position_t target) {
+  return target.i == defender.i && target.j < defender.j;
+}
+
+bool up(position_t defender, position_t target) {
+  return target.i < defender.i && target.j == defender.j;
+}
+
+bool up_right(position_t defender, position_t target) {
+  return target.i < defender.i && target.j > defender.j;
+}
+
+bool up_left(position_t defender, position_t target) {
+  return target.i < defender.i && target.j < defender.j;
+}
+
+// bool down(position_t defender, position_t target) {
+//   return target.i > defender.i && target.j == defender.j;
+// }
+
+bool down_right(position_t defender, position_t target) {
+  return target.i > defender.i && target.j > defender.j;
+}
+
+bool down_left(position_t defender, position_t target) {
+  return target.i > defender.i && target.j < defender.j;
+}
+
+/*----------------------------------------------------------------------------*/
+
+direction_t follow_target(position_t defender, position_t target){
+  if(right(defender, target)){
+    return DIR_RIGHT;
+  }
+  else if(left(defender, target)){
+   return DIR_LEFT;
+  }
+  else if(up(defender, target)){
+   return DIR_UP;
+  }
+  else if(up_right(defender, target)){
+   return DIR_UP_RIGHT;
+  }
+  else if(up_left(defender, target)){
+   return DIR_UP_LEFT;
+  }
+  else if(down_right(defender, target)){
+   return DIR_DOWN_RIGHT;
+  }
+  else if(down_left(defender, target)){
+   return DIR_DOWN_LEFT;
+  }
+  return DIR_DOWN;
+}
+
+/*----------------------------------------------------------------------------*/
+
 /*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
 direction_t execute_defender_strategy(
     position_t defender_position, Spy attacker_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(defender_position);
-  UNUSED(attacker_spy);
+  static int round = 0;
+  static position_t target_position = {0, 0};
+  double random_chance;
 
-  // TODO: Implement Defender logic here
-  return DIR_LEFT;
+  round++;
+  random_chance = (double)random()/ (double)RAND_MAX;
+
+  if(round == 10) {
+    target_position = get_spy_position(attacker_spy);
+  }
+
+  if(random_chance < 0.2) {
+    return DIR_UP;
+  }
+  else if(random_chance < 0.4) {
+    return DIR_DOWN;
+  }
+  
+  return follow_target(defender_position, target_position);
 }
 
 /*----------------------------------------------------------------------------*/
