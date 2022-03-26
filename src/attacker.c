b01bea204d2b20993949344ff2b752eca6210200
davimp <davimp2009@gmail.com>
2022-03-26 18:25:24 -0300

    Add attacker strategy

--- src/attacker.c
+++ src/attacker.c
@@ -1,5 +1,6 @@
 // Standard headers
 #include <stdio.h>
+#include <stdlib.h>
 
 // Internal headers
 #include "direction.h"
@@ -13,17 +14,52 @@
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
 
 /*----------------------------------------------------------------------------*/
+/*                              PRIVATE FUNCTIONS                             */
+/*----------------------------------------------------------------------------*/
+
+// Utils
+
+bool down(position_t attacker, position_t threat) {
+  return threat.i > attacker.i;
+}
+
+/*----------------------------------------------------------------------------*/
+
+direction_t avoid_threat_and_go_right(position_t attacker, position_t threat){
+  if(equal_positions(attacker, threat)){
+   return DIR_DOWN;
+  }
+  else if(down(attacker, threat)){
+   return DIR_UP_RIGHT;
+  }
+  return DIR_DOWN_RIGHT;
+}
+
+/*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
 direction_t execute_attacker_strategy(
     position_t attacker_position, Spy defender_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(attacker_position);
-  UNUSED(defender_spy);
+  static int round = 0;
+  static position_t threat_position = {100, 100};
+  double random_chance;
 
-  // TODO: Implement Attacker logic here
-  return DIR_RIGHT;
+  round++;
+  random_chance = (double)random()/ (double)RAND_MAX;
+
+  if(round == 10) {
+    threat_position = get_spy_position(defender_spy);
+  }
+
+  if(random_chance < 0.2) {
+    return DIR_UP;
+  }
+  else if(random_chance < 0.4) {
+    return DIR_DOWN;
+  }
+
+  return avoid_threat_and_go_right(attacker_position, threat_position);
 }
 
 /*----------------------------------------------------------------------------*/
